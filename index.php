ec83f2fb26445d31c88a9125386e7cdf592bc19f
Fahry <fahryfahreza330@gmail.com>
2025-01-20 14:03:29 +0700

    Update ketika di klik lihat gambar menampilkan gambar di tab baru

--- index.php
+++ index.php
@@ -6,12 +6,14 @@ $limit = 10;
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
 $offset = ($page - 1) * $limit;
 
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+
 $test = new Test();
 
-$query = "SELECT * FROM tb_develop LIMIT $limit OFFSET $offset";
+$query = "SELECT * FROM tb_develop WHERE nomor LIKE '%$search%' OR status LIKE '%$search%' OR ip_address LIKE '%$search%' OR tag_value LIKE '%$search%' OR file_name LIKE '%$search%' OR date LIKE '%$search%' LIMIT $limit OFFSET $offset";
 $result = $test->conn->query($query);
 
-$total_query = "SELECT COUNT(*) FROM tb_develop";
+$total_query = "SELECT COUNT(*) FROM tb_develop WHERE nomor LIKE '%$search%' OR status LIKE '%$search%' OR ip_address LIKE '%$search%' OR tag_value LIKE '%$search%' OR file_name LIKE '%$search%' OR date LIKE '%$search%'";
 $total_result = $test->conn->query($total_query);
 $total_rows = $total_result->fetch_row()[0];
 $total_pages = ceil($total_rows / $limit);
@@ -26,7 +28,14 @@ $total_pages = ceil($total_rows / $limit);
 </head>
 <body>
     <h1>Data Table</h1>
-    <a href="form.php"><button>Tambah Data</button></a> 
+
+    <div class="table-container">
+        <a href="form.php"><button>Tambah Data</button></a>
+        <form class="search-form" method="GET" action="index.php">
+            <input type="text" name="search" placeholder="Search..." value="<?php echo htmlspecialchars($search); ?>">
+            <button type="submit">Search</button>
+        </form>
+    </div>
     <table>
         <tr>
             <th>Nomor</th>
@@ -45,37 +54,25 @@ $total_pages = ceil($total_rows / $limit);
                 <td><?php echo $row['tag_value']; ?></td>
                 <td><?php echo $row['file_name']; ?></td>
                 <td><?php echo $row['date']; ?></td>
-                <td><button onclick="showImage('<?php echo $row['file_name']; ?>')">Lihat Gambar</button></td>
+                <td>
+                    <a href="uploads/<?php echo $row['file_name']; ?>" target="_blank">
+                        <button>Lihat Gambar</button>
+                    </a>
+                </td>
             </tr>
         <?php endwhile; ?>
     </table>
 
     <div class="pagination">
         <?php if ($page > 1): ?>
-            <a href="index.php?page=<?php echo $page - 1; ?>">&laquo; Previous</a>
+            <a href="index.php?page=<?php echo $page - 1; ?>&search=<?php echo urlencode($search); ?>">&laquo; Previous</a>
         <?php endif; ?>
         <?php for ($i = 1; $i <= $total_pages; $i++): ?>
-            <a href="index.php?page=<?php echo $i; ?>" <?php if ($i == $page) echo 'class="active"'; ?>><?php echo $i; ?></a>
+            <a href="index.php?page=<?php echo $i; ?>&search=<?php echo urlencode($search); ?>" <?php if ($i == $page) echo 'class="active"'; ?>><?php echo $i; ?></a>
         <?php endfor; ?>
         <?php if ($page < $total_pages): ?>
-            <a href="index.php?page=<?php echo $page + 1; ?>">Next &raquo;</a>
+            <a href="index.php?page=<?php echo $page + 1; ?>&search=<?php echo urlencode($search); ?>">Next &raquo;</a>
         <?php endif; ?>
     </div>
-
-    <div id="imageModal" class="modal">
-        <div class="modal-content">
-            <span class="close" onclick="document.getElementById('imageModal').style.display='none'">&times;</span>
-            <img id="modalImage" src="" alt="Image" style="width:100%">
-        </div>
-    </div>
-
-    <script>
-        function showImage(fileName) {
-            var modal = document.getElementById('imageModal');
-            var modalImg = document.getElementById("modalImage");
-            modal.style.display = "block";
-            modalImg.src = "uploads/" + fileName; 
-        }
-    </script>
 </body>
 </html>
